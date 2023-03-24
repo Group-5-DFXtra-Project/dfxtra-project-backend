4a88a366d83f39b75aaf27b8130a4658c67db807
Marta Conti <marta.conti.lorenzo@gmail.com>
2023-03-24 15:13:26 +0000

    cors

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 import userRoutes from './routes/users.js';
 import authRoutes from './routes/auths.js';
 
@@ -21,7 +20,7 @@ const connect = () => {
             throw err;
         });
 };
-
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/users", userRoutes);
