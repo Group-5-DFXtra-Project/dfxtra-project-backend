9cb83722dd8429068c7774de85e006c2574ac7a3
sharifgk <sharif_khan@live.co.uk>
2023-03-29 13:38:04 +0100

    profileHeader backend stuff working

--- controllers/profileinfo.js
+++ controllers/profileinfo.js
@@ -21,3 +21,25 @@ export const addCertification = async (req, res, next) => {
 		next(e);
 	}
 };
+
+export const updateProfileHeader = async (req, res, next) => {
+	try {
+		const userId = req.userId;
+		const updatedProfileHeader = req.body;
+
+		const profileInfo = await ProfileInfo.findOneAndUpdate(
+			{ user: userId },
+			{ profileHeader: updatedProfileHeader },
+			{ new: true } // returns updated document
+		);
+
+		if (!profileInfo) {
+			return res.status(404).json({ message: 'user not found' });
+		}
+
+		res.status(200).json(profileInfo.profileHeader);
+
+	} catch (e) {
+		next(e);
+	}
+};
--- routes/profileInfoRoute.js
+++ routes/profileInfoRoute.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { getProfileInfo } from '../controllers/user.js';
 import { verifyToken } from '../middlewares/authJwt.js';
-import { addCertification } from '../controllers/profileinfo.js';
+import { addCertification, updateProfileHeader } from '../controllers/profileinfo.js';
 
 const router = express.Router();
 
 router.get('/', verifyToken, getProfileInfo);
 router.put('/certifications', verifyToken, addCertification);
+router.put('/profileheader', verifyToken, updateProfileHeader);
 
 export default router;
